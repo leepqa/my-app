0ef2cb225f0ff2846dc033b497654dbd6d51574a
leepq <mikita.lipavetski@innowise-group.com>
2021-12-03 15:44:48 +0300

    fist commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,38 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'my-app';
+  public title = 'Hello';
+  public counter = 0;
+  public anyCounter = 0;
+  public color = 'pink';
+  public otherColor = 'brown';
+  public letters = '0123456789ABCDEF';
+  public changeColorTitle = 'HI HO';
+
+
+  constructor() { }
+
+
+  public count(): void {
+    this.counter++;
+
+  }
+  public anyCount(): void {
+    this.anyCounter++;
+  }
+
+  getOtherRandomColor(): void {
+    this.otherColor = '#';
+    for (var i = 0; i < 6; i++) {
+      this.otherColor += this.letters[Math.floor(Math.random() * 16)];
+    }
+  }
+
+  getRandomColor(): void {
+    this.color = '#';
+    for (var i = 0; i < 6; i++) {
+      this.color += this.letters[Math.floor(Math.random() * 16)];
+    }
+  }
 }
+
